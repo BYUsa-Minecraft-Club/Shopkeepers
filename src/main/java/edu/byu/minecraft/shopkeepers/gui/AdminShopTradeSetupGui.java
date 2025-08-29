008a8f8bd28c76e5c1f1136ee1aa858dffbf8993
Michael Davenport <mdaven19@byu.edu>
2025-08-29 16:42:44 -0600

    feat: show uses of each trade in admin trade setup menu

--- src/main/java/edu/byu/minecraft/shopkeepers/gui/AdminShopTradeSetupGui.java
+++ src/main/java/edu/byu/minecraft/shopkeepers/gui/AdminShopTradeSetupGui.java
@@ -1,8 +1,12 @@
 package edu.byu.minecraft.shopkeepers.gui;
 
+import edu.byu.minecraft.Shopkeepers;
+import edu.byu.minecraft.shopkeepers.data.ShopkeeperData;
+import edu.byu.minecraft.shopkeepers.data.TradeData;
 import eu.pb4.sgui.api.elements.GuiElementBuilder;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.screen.slot.Slot;
@@ -10,26 +14,28 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.village.VillagerProfession;
 
-import java.util.UUID;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class AdminShopTradeSetupGui extends TradeSetupGui {
 
 
     public AdminShopTradeSetupGui(ServerPlayerEntity player, Entity shopkeeper) {
-        super(ScreenHandlerType.GENERIC_9X4, player, shopkeeper, 45);
+        super(ScreenHandlerType.GENERIC_9X5, player, shopkeeper, 45);
         this.setTitle(Text.of("Admin shop setup"));
     }
 
     protected void setupSlots() {
         setupPage(0);
 
-        babyToggle(28);
-        setName(29);
-        mobOptions(30);
-        this.setSlot(31, GuiUtils.EMPTY_SLOT);
-        disbandShopkeeper(32);
-        this.setSlot(33, GuiUtils.EMPTY_SLOT);
-        openTradeMenu(34);
+        babyToggle(37);
+        setName(38);
+        mobOptions(39);
+        this.setSlot(40, GuiUtils.EMPTY_SLOT);
+        disbandShopkeeper(41);
+        this.setSlot(42, GuiUtils.EMPTY_SLOT);
+        openTradeMenu(43);
     }
 
     private void setupPage(int page) {
@@ -37,11 +43,33 @@ public class AdminShopTradeSetupGui extends TradeSetupGui {
             this.setSlotRedirect(i, new Slot(tradeItems, (27 * page) + i, 0, 0));
         }
 
-        this.setSlot(27, page > 0 ?
+        ShopkeeperData shopkeeperData = Shopkeepers.getData().getShopkeeperData().get(shopkeeper.getUuid());
+        for(int i = 0; i < 9; i++) {
+            int trade = (9 * page) + i;
+            if(shopkeeperData.trades().size() > trade && shopkeeperData.trades().get(trade) != null) {
+                TradeData tradeData = shopkeeperData.trades().get(trade);
+                List<Text> users = tradeData.uses().entrySet().stream()
+                        .map(entry -> Map.entry(Shopkeepers.getData().getPlayers().get(entry.getKey()),
+                                entry.getValue())).sorted((o1, o2) -> {
+                            if (Objects.equals(o1.getValue(), o2.getValue())) return o1.getKey().compareTo(o2.getKey());
+                            return o2.getValue().compareTo(o1.getValue());
+                        }).map(entry -> Text.of(String.format("%s: %d", entry.getKey(), entry.getValue())))
+                        .toList();
+                int totalUses = tradeData.uses().values().stream().mapToInt(v -> v).sum();
+
+                setSlot(27 + i, new GuiElementBuilder(totalUses == 0 ? Items.BOOK : Items.WRITTEN_BOOK)
+                        .setItemName(Text.of(String.format("Trade used %d times", totalUses)))
+                        .setLore(users));
+            } else {
+                setSlot(27 + i, new GuiElementBuilder(Items.PAPER).setItemName(Text.of("Create trade to track uses")));
+            }
+        }
+
+        this.setSlot(36, page > 0 ?
                 new GuiElementBuilder(Items.ARROW).setItemName(Text.of("Previous Page")).setCallback(() -> setupPage(page - 1)).build() :
                 GuiUtils.EMPTY_SLOT);
 
-        this.setSlot(35, page < 4 ?
+        this.setSlot(44, page < 4 ?
                 new GuiElementBuilder(Items.ARROW).setItemName(Text.of("Next Page")).setCallback(() -> setupPage(page + 1)).build() :
                 GuiUtils.EMPTY_SLOT);
     }
