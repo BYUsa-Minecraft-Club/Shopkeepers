35945794004539dc601a61d0192af776824da106
Michael Davenport <mdaven19@byu.edu>
2025-09-09 20:50:06 -0600

    fix allay not holding armor issue

--- src/main/java/edu/byu/minecraft/shopkeepers/customization/equipment/AllayCustomizations.java
+++ src/main/java/edu/byu/minecraft/shopkeepers/customization/equipment/AllayCustomizations.java
@@ -1,11 +1,11 @@
 package edu.byu.minecraft.shopkeepers.customization.equipment;
 
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.passive.AllayEntity;
 import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
 
-public class AllayCustomizations extends MobEquipmentCustomizations<AllayEntity> {
-    protected AllayCustomizations() {
-        super(EquipmentSlot.MAINHAND, "held item", Items.CAKE);
+public class AllayCustomizations extends HandEquipmentCustomizations<AllayEntity> {
+    public AllayCustomizations() {
+        super(Hand.MAIN_HAND, "held item", Items.CAKE);
     }
 }
