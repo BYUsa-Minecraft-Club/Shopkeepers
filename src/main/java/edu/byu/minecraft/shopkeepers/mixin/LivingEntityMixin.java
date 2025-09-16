7059da060ccd6344b5512f5205affc43fc93b450
Michael Davenport <mdaven19@byu.edu>
2025-09-16 14:51:56 -0600

    fix shopkeeper death prevented warning message - include usernames rather than uuids

--- src/main/java/edu/byu/minecraft/shopkeepers/mixin/LivingEntityMixin.java
+++ src/main/java/edu/byu/minecraft/shopkeepers/mixin/LivingEntityMixin.java
@@ -10,6 +10,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Comparator;
+import java.util.UUID;
+import java.util.function.Function;
+
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
 
@@ -20,7 +24,9 @@ public class LivingEntityMixin {
             self.setHealth(Float.MAX_VALUE);
             String warning = String.format("Shopkeeper entity %s owned by %s was attempted to be killed (likely by a command). " +
                             "Please disband the shop first. Location: X: %.0f, Y:%.0f, Z:%.0f",
-                    self.getName().getString(), Shopkeepers.getData().getShopkeeperData().get(self.getUuid()).owners(),
+                    self.getName().getString(),
+                    Shopkeepers.getData().getShopkeeperData().get(self.getUuid()).owners().stream().map(
+                            uuid -> Shopkeepers.getData().getPlayers().get(uuid)).sorted(String::compareToIgnoreCase).toList(),
                     self.getPos().getX(), self.getPos().getY(), self.getPos().getZ());
 
             if(self.getServer() != null) {
